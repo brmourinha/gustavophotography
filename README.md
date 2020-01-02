1fb9732bde349a907dc9085f18f16ed3948a259a
MusDev <brmourinha@gmail.com>
2020-01-02 11:36:10 +0000

    Version 1

---
+++ README.md
@@ -0,0 +1,1 @@
+Gustavo Photography Web Page
---
+++ app.js
@@ -0,0 +1,9 @@
+const gallery = document.querySelector("#gallery");
+const images = gallery.querySelectorAll("img");
+
+images.forEach(image => {
+  image.addEventListener("mouseover", () => {
+    let random = Math.floor(Math.random() * 13) + 1;
+    image.setAttribute("src", `./img/image${random}.jpg`);
+  });
+});
