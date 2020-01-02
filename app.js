4f0126925880843085327a8d83bc726b164c2148
Bruno Mourinha <brmourinha@gmail.com>
2020-01-02 12:08:13 +0000

    gallery changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const gallery = document.querySelector("#gallery");
-const images = gallery.querySelectorAll("img");
-
+const gallery = document.querySelector('#gallery');
+const images = gallery.querySelectorAll('img');
+/*
 images.forEach(image => {
   image.addEventListener("mouseover", () => {
     let random = Math.floor(Math.random() * 13) + 1;
     image.setAttribute("src", `./img/image${random}.jpg`);
   });
-});
+});*/
