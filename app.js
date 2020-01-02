19122bac728b8291b0db81863d5ad26256e770a9
Bruno Mourinha <brmourinha@gmail.com>
2020-01-02 13:31:16 +0000

    Responsive Web

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Gallery */
+
 const gallery = document.querySelector('#gallery');
 const images = gallery.querySelectorAll('img');
 /*
@@ -7,3 +9,20 @@ images.forEach(image => {
     image.setAttribute("src", `./img/image${random}.jpg`);
   });
 });*/
+
+/* Nav Menu */
+const nav = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger');
+const links = nav.querySelectorAll('a');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+  burger.classList.toggle('toggle');
+});
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    burger.classList.toggle('toggle');
+  });
+});
