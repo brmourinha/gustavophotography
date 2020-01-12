ec762182688451e8d04599e8bab3bb003f6978a1
MusDev <brmourinha@gmail.com>
2020-01-12 14:41:09 +0000

    Gallery Zoom Funcionality

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 /* Gallery */
+const gallery = document.querySelector("#gallery");
+const images = gallery.querySelectorAll("img");
 
-const gallery = document.querySelector('#gallery');
-const images = gallery.querySelectorAll('img');
-/*
-images.forEach(image => {
-  image.addEventListener("mouseover", () => {
-    let random = Math.floor(Math.random() * 13) + 1;
-    image.setAttribute("src", `./img/image${random}.jpg`);
-  });
-});*/
+document.body.addEventListener("click", e => {
+  if (!e.target.classList.contains("zoom")) {
+    const elem = document.querySelector(".zoom");
+
+    if (elem !== null && elem.classList.contains("zoom")) {
+      elem.parentNode.removeChild(elem);
+    }
+  }
+  if (e.target.classList.contains("image")) {
+    let cln = e.target.cloneNode(true);
+    cln.classList.add("zoom");
+    document.body.appendChild(cln);
+  }
+});
 
 /* Nav Menu */
-const nav = document.querySelector('.nav-links');
-const burger = document.querySelector('.burger');
-const links = nav.querySelectorAll('a');
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
 
-burger.addEventListener('click', () => {
-  nav.classList.toggle('nav-open');
-  burger.classList.toggle('toggle');
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
 });
 
 links.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
-    burger.classList.toggle('toggle');
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
   });
 });
